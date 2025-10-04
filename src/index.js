9b27e8e36e00e8f45333b20be0aa47f0d58d862d
IsmailHossen87 <ismailhosen8757@gmail.com>
2025-10-04 13:13:27 +0600

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import path from "path";
 import { fileURLToPath } from "url";
 
+// we found file and folders path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -11,15 +12,18 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// public to static folder serve
 app.use(express.static(path.join(__dirname, "public"))); 
+
+
 
 io.on("connection", (socket) => {
   console.log("A user connected ✅");
 
-
-  socket.on("newItem", (data) => {
-    io.emit("newItem", data);
+  socket.on("secondItem", (data) => {
+    io.emit("secondItem", data);
   });
+
 
   socket.on("disconnect", () => {
     console.log("User disconnected ❌");
